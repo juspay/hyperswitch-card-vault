e5883885e7d3911144d82a7ca809ffe32d5d7a13
Nishant Joshi <nishant.joshi@juspay.in>
2023-10-28 16:04:47 +0530

    chore: minor fixes

--- src/crypto/sha.rs
+++ src/crypto/sha.rs
@@ -6,7 +6,7 @@ impl super::Encode<Vec<u8>, Vec<u8>> for Sha512 {
     type ReturnType<T> = error_stack::Result<T, error::CryptoError>;
 
     fn encode(&self, input: Vec<u8>) -> Self::ReturnType<Vec<u8>> {
-        let digest = ring::digest::digest(&ring::digest::SHA256, &input);
+        let digest = ring::digest::digest(&ring::digest::SHA512, &input);
         Ok(digest.as_ref().to_vec())
     }
 }
