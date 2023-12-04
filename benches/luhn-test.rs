08ab42d41c2173e8b5f821fbcc54816a8e2dc2a3
Nishant Joshi <nishant.joshi@juspay.in>
2023-12-04 14:09:35 +0530

    chore: fix clippy

--- benches/luhn-test.rs
+++ benches/luhn-test.rs
@@ -1,10 +1,20 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use std::fmt::Write;
 use tartarus::validations::luhn_on_string;
 
+// fn card_number_generator() -> String {
+//     (0..16)
+//         .map(|_| format!("{}", rand::random::<u8>() % 10))
+//         .collect()
+// }
+
+#[allow(clippy::expect_used)]
 fn card_number_generator() -> String {
-    (0..16)
-        .map(|_| format!("{}", rand::random::<u8>() % 10))
-        .collect()
+    (0..16).fold(String::new(), |mut acc, _| {
+        write!(&mut acc, "{}", rand::random::<u8>() % 10)
+            .expect("Failed to write to string buffer");
+        acc
+    })
 }
 
 pub fn criterion_luhn(c: &mut Criterion) {
