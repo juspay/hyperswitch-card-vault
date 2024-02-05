3992cff602a214edee8f0f0fd0c9d852db7960ca
Nishant Joshi <nishant.joshi@juspay.in>
2024-02-05 18:57:47 +0530

    feat(hmac): add implementation for hmac-sha512

---
+++ benches/hashing.rs
@@ -0,0 +1,54 @@
+#![allow(clippy::expect_used)]
+#![allow(clippy::missing_panics_doc)]
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use tartarus::crypto::{sha::HmacSha512, Encode};
+
+const ITERATION: u32 = 14;
+
+criterion_main!(benches);
+criterion_group!(benches, criterion_hmac_sha512);
+
+macro_rules! const_iter {
+    ($algo:ident, $body:block, $key:ident, $($ty:expr),*) => {
+        $(
+            let $algo = $ty($key.clone());
+            $body
+        )*
+    };
+}
+
+pub fn criterion_hmac_sha512(c: &mut Criterion) {
+    let key: masking::Secret<_> = (0..1000)
+        .map(|_| rand::random::<u8>())
+        .collect::<Vec<_>>()
+        .into();
+
+
+    const_iter!(
+        algo,
+        {
+            let mut group = c.benchmark_group(format!("{}", algo));
+            (1..ITERATION).for_each(|po| {
+                let max: u64 = (2_u64).pow(po);
+                let value = (0..max).map(|_| rand::random::<u8>()).collect::<Vec<_>>();
+                let hashed = algo.encode(value.clone()).unwrap();
+                group.throughput(criterion::Throughput::Bytes(max));
+                group.bench_with_input(
+                    criterion::BenchmarkId::from_parameter(format!("{}", max)),
+                    &value,
+                    |b, value| {
+                        b.iter(|| {
+                            black_box(algo.encode(black_box(value.clone())).unwrap() == hashed)
+                        })
+                    },
+                );
+            })
+        },
+        key,
+        HmacSha512::<1>::new,
+        HmacSha512::<10>::new,
+        HmacSha512::<100>::new,
+        HmacSha512::<1000>::new
+    );
+}
