71a994c855022fd2ac23d9c4a3a162a1b4606cb3
Chethan <chethan.rao@juspay.in>
2024-05-14 14:47:16 +0530

    create separate methods for each validation

--- benches/luhn-test.rs
+++ benches/luhn-test.rs
@@ -1,12 +1,10 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use std::fmt::Write;
-use tartarus::validations::validate_card_number;
+use tartarus::validations::{luhn, MAX_CARD_NUMBER_LENGTH};
 
 #[allow(clippy::expect_used)]
-fn card_number_generator() -> String {
-    (0..16).fold(String::new(), |mut acc, _| {
-        write!(&mut acc, "{}", rand::random::<u8>() % 10)
-            .expect("Failed to write to string buffer");
+fn card_number_generator() -> Vec<u8> {
+    (0..16).fold(Vec::with_capacity(MAX_CARD_NUMBER_LENGTH), |mut acc, _| {
+        acc.push(rand::random::<u8>() % 10);
         acc
     })
 }
@@ -16,11 +14,11 @@ pub fn criterion_luhn(c: &mut Criterion) {
         b.iter(|| black_box(card_number_generator()))
     });
     c.bench_function("luhn-validation", |b| {
-        b.iter(|| black_box(validate_card_number(&black_box(card_number_generator()))))
+        b.iter(|| black_box(luhn(&black_box(card_number_generator()))))
     });
     let card_number = card_number_generator();
     c.bench_function("luhn", |b| {
-        b.iter(|| black_box(validate_card_number(black_box(&card_number))))
+        b.iter(|| black_box(luhn(black_box(&card_number))))
     });
 }
 
--- src/validations.rs
+++ src/validations.rs
@@ -3,21 +3,30 @@ use crate::error::ApiError;
 ///
 /// Minimum limit of a card number will not deceed 8 by ISO standards
 ///
-const MIN_CARD_NUMBER_LENGTH: usize = 8;
+pub const MIN_CARD_NUMBER_LENGTH: usize = 8;
 
 ///
 /// Maximum limit of a card number will not exceed 19 by ISO standards
 ///
-const MAX_CARD_NUMBER_LENGTH: usize = 19;
+pub const MAX_CARD_NUMBER_LENGTH: usize = 19;
+
+pub fn sanitize_card_number(card_number: &str) -> Result<bool, ApiError> {
+    let card_number = card_number.split_whitespace().collect::<String>();
+
+    let is_card_number_valid = Ok(card_number.as_str())
+        .and_then(validate_card_number_chars)
+        .and_then(validate_card_number_length)
+        .map(|number| luhn(&number))?;
+
+    Ok(is_card_number_valid)
+}
 
 ///
 /// # Panics
 ///
 /// Never, as a single character will never be greater than 10, or `u8`
 ///
-pub fn validate_card_number(number: &str) -> Result<bool, ApiError> {
-    let number = number.split_whitespace().collect::<String>();
-
+pub fn validate_card_number_chars(number: &str) -> Result<Vec<u8>, ApiError> {
     let data = number.chars().try_fold(
         Vec::with_capacity(MAX_CARD_NUMBER_LENGTH),
         |mut data, character| {
@@ -35,11 +44,15 @@ pub fn validate_card_number(number: &str) -> Result<bool, ApiError> {
         },
     )?;
 
-    Ok(
-        (data.len() >= MIN_CARD_NUMBER_LENGTH && data.len() <= MAX_CARD_NUMBER_LENGTH)
-            .then(|| luhn(&data))
-            .unwrap_or(false),
-    )
+    Ok(data)
+}
+
+pub fn validate_card_number_length(number: Vec<u8>) -> Result<Vec<u8>, ApiError> {
+    if number.len() >= MIN_CARD_NUMBER_LENGTH && number.len() <= MAX_CARD_NUMBER_LENGTH {
+        Ok(number)
+    } else {
+        Err(ApiError::ValidationError("invalid card number length"))
+    }
 }
 
 #[allow(clippy::as_conversions)]
