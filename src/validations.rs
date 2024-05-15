f25efeb5ac44d4ea021dd9535d5b166b7f10826d
Chethan Rao <70657455+Chethan-rao@users.noreply.github.com>
2024-05-15 13:15:32 +0530

    fix: address non-digit character cases in card number validation (#93)

--- src/validations.rs
+++ src/validations.rs
@@ -1,29 +1,58 @@
+use crate::error::ApiError;
+
+///
+/// Minimum limit of a card number will not deceed 8 by ISO standards
+///
+pub const MIN_CARD_NUMBER_LENGTH: usize = 8;
+
 ///
 /// Maximum limit of a card number will not exceed 19 by ISO standards
 ///
-const CARD_NUMBER_LENGTH: usize = 20;
+pub const MAX_CARD_NUMBER_LENGTH: usize = 19;
 
-#[allow(clippy::expect_used)]
+pub fn sanitize_card_number(card_number: &str) -> Result<bool, ApiError> {
+    let card_number = card_number.split_whitespace().collect::<String>();
+
+    let is_card_number_valid = Ok(card_number.as_str())
+        .and_then(validate_card_number_chars)
+        .and_then(validate_card_number_length)
+        .map(|number| luhn(&number))?;
+
+    Ok(is_card_number_valid)
+}
+
 ///
 /// # Panics
 ///
 /// Never, as a single character will never be greater than 10, or `u8`
 ///
-pub fn luhn_on_string(number: &str) -> bool {
-    let data: Vec<u8> = number
-        .chars()
-        .filter_map(|value| value.to_digit(10))
-        .map(|value| {
-            value
-                .try_into()
-                .expect("error while converting a single character to u8")
-        }) // safety, a single character will never be greater
-        // `u8`
-        .collect();
+pub fn validate_card_number_chars(number: &str) -> Result<Vec<u8>, ApiError> {
+    let data = number.chars().try_fold(
+        Vec::with_capacity(MAX_CARD_NUMBER_LENGTH),
+        |mut data, character| {
+            data.push(
+                #[allow(clippy::expect_used)]
+                character
+                    .to_digit(10)
+                    .ok_or(ApiError::ValidationError(
+                        "invalid character found in card number",
+                    ))?
+                    .try_into()
+                    .expect("error while converting a single character to u8"), // safety, a single character will never be greater `u8`
+            );
+            Ok::<Vec<u8>, ApiError>(data)
+        },
+    )?;
 
-    (data.len() < CARD_NUMBER_LENGTH)
-        .then(|| luhn(&data))
-        .unwrap_or(false)
+    Ok(data)
+}
+
+pub fn validate_card_number_length(number: Vec<u8>) -> Result<Vec<u8>, ApiError> {
+    if number.len() >= MIN_CARD_NUMBER_LENGTH && number.len() <= MAX_CARD_NUMBER_LENGTH {
+        Ok(number)
+    } else {
+        Err(ApiError::ValidationError("invalid card number length"))
+    }
 }
 
 #[allow(clippy::as_conversions)]
