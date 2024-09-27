16ade8bb27dc3c588bba409a4d4695b3bb045538
Nishant Joshi <nishant.joshi@juspay.in>
2024-09-27 12:49:17 +0530

    chore: fix storage invocation

--- src/app.rs
+++ src/app.rs
@@ -45,7 +45,7 @@ impl TenantAppState {
         tenant_config: TenantConfig,
         api_client: ApiClient,
     ) -> error_stack::Result<Self, error::ConfigurationError> {
-        let db = storage::Storage::new(&global_config.database, &tenant_config.tenant_id)
+        let db = storage::Storage::new(&global_config.database, &tenant_config.schema)
             .await
             .map(
                 #[cfg(feature = "caching")]
