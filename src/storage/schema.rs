a995474492f1882a76dbec695d37cb64a5ffcce3
Jagan Elavarasan <jaganelavarasan@gmail.com>
2025-03-21 15:16:11 +0530

    initial commit

--- src/storage/schema.rs
+++ src/storage/schema.rs
@@ -1,6 +1,28 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    card_info (card_isin) {
+        card_isin -> Text,
+        card_switch_provider -> Text,
+        card_type -> Nullable<Text>,
+        card_sub_type -> Nullable<Text>,
+        card_sub_type_category -> Nullable<Text>,
+        card_issuer_country -> Nullable<Text>,
+        country_code -> Nullable<Text>,
+        extended_card_type -> Nullable<Text>,
+    }
+}
+
+diesel::table! {
+    emi_bank_code (id) {
+        id -> Int8,
+        emi_bank -> Text,
+        juspay_bank_code_id -> Nullable<Int8>,
+        last_updated -> Nullable<Timestamp>,
+    }
+}
+
+diesel::table! {
     entity (entity_id) {
         id -> Int4,
         #[max_length = 255]
@@ -8,6 +30,15 @@ diesel::table! {
         #[max_length = 255]
         enc_key_id -> Varchar,
         created_at -> Timestamp,
+    }
+}
+
+diesel::table! {
+    feature (id) {
+        id -> Int4,
+        enabled -> Bool,
+        name -> Text,
+        merchant_id -> Nullable<Text>,
     }
 }
 
@@ -21,12 +52,44 @@ diesel::table! {
 }
 
 diesel::table! {
+    gateway_bank_emi_support (id) {
+        id -> Int8,
+        gateway -> Text,
+        bank -> Text,
+        juspay_bank_code_id -> Nullable<Int8>,
+        scope -> Nullable<Text>,
+    }
+}
+
+diesel::table! {
+    gateway_card_info (id) {
+        id -> Int8,
+        isin -> Nullable<Text>,
+        gateway -> Nullable<Text>,
+        card_issuer_bank_name -> Nullable<Text>,
+        auth_type -> Nullable<Text>,
+        juspay_bank_code_id -> Nullable<Int8>,
+        disabled -> Nullable<Bool>,
+        validation_type -> Nullable<Text>,
+        payment_method_type -> Nullable<Text>,
+    }
+}
+
+diesel::table! {
     hash_table (hash_id) {
         id -> Int4,
         #[max_length = 255]
         hash_id -> Varchar,
         data_hash -> Bytea,
         created_at -> Timestamp,
+    }
+}
+
+diesel::table! {
+    juspay_bank_code (id) {
+        id -> Int8,
+        bank_code -> Text,
+        bank_name -> Text,
     }
 }
 
@@ -58,6 +121,34 @@ diesel::table! {
 }
 
 diesel::table! {
+    merchant_gateway_account (id) {
+        id -> Int8,
+        account_details -> Text,
+        gateway -> Text,
+        merchant_id -> Text,
+        payment_methods -> Nullable<Text>,
+        supported_payment_flows -> Nullable<Text>,
+        disabled -> Nullable<Bool>,
+        reference_id -> Nullable<Text>,
+        supported_currencies -> Nullable<Text>,
+        gateway_identifier -> Nullable<Text>,
+        gateway_type -> Nullable<Text>,
+        supported_txn_type -> Nullable<Text>,
+    }
+}
+
+diesel::table! {
+    user_eligibility_info (id) {
+        id -> Text,
+        flow_type -> Text,
+        identifier_name -> Text,
+        identifier_value -> Text,
+        provider_name -> Text,
+        disabled -> Nullable<Bool>,
+    }
+}
+
+diesel::table! {
     vault (entity_id, vault_id) {
         id -> Int4,
         #[max_length = 255]
@@ -70,11 +161,23 @@ diesel::table! {
     }
 }
 
+diesel::joinable!(emi_bank_code -> juspay_bank_code (juspay_bank_code_id));
+diesel::joinable!(gateway_bank_emi_support -> juspay_bank_code (juspay_bank_code_id));
+diesel::joinable!(gateway_card_info -> card_info (isin));
+
 diesel::allow_tables_to_appear_in_same_query!(
+    card_info,
+    emi_bank_code,
     entity,
+    feature,
     fingerprint,
+    gateway_bank_emi_support,
+    gateway_card_info,
     hash_table,
+    juspay_bank_code,
     locker,
     merchant,
+    merchant_gateway_account,
+    user_eligibility_info,
     vault,
 );
