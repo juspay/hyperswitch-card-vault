c54bf7a4b8890b9dafcc543bc33624405cb6994d
Nishant Joshi <nishant.joshi@juspay.in>
2025-04-23 15:33:14 +0530

    chore: move the dev notification at the start of the main function

--- src/bin/locker.rs
+++ src/bin/locker.rs
@@ -3,6 +3,11 @@ use tartarus::{logger, tenant::GlobalAppState};
 #[allow(clippy::expect_used)]
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    if cfg!(feature = "dev") {
+        eprintln!("This is a dev build, not for production use");
+    }
+
     let mut global_config =
         tartarus::config::GlobalConfig::new().expect("Failed while parsing config");
 
@@ -23,9 +28,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let global_app_state = GlobalAppState::new(global_config).await;
 
-    if cfg!(feature = "dev") {
-        eprintln!("This is a dev build, not for production use");
-    }
 
     tartarus::app::server_builder(global_app_state)
         .await
